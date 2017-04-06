afc1047dfd3c96e6be5f8886504e35b6792069c5
turbobeast <georgemichaelfaust@gmail.com>
2017-04-06 15:12:58 -0400

    changed thunks, to actually rely on middleware

--- src/actions.js
+++ src/actions.js
@@ -9,7 +9,7 @@ import { apiCall } from './api/api'
 
 export const setSearchTerm = (text) => ({ type: CHANGE_SEARCHTERM, payload: text })
 
-export const requestRobots = (dispatch) => {
+export const requestRobots = () => (dispatch) => {
   dispatch({ type: REQUEST_ROBOTS_PENDING })
   apiCall('https://jsonplaceholder.typicode.com/users')
     .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
